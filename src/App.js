cde4828c1faa529954e48d6a976ec787d5208a87
ShadidIntisar99 <shadidintisar@gmail.com>
2021-02-09 14:46:38 +0600

    sign in and sign up page with routing

--- src/App.js
+++ src/App.js
@@ -1,60 +1,46 @@
-import { AppBar, Avatar, Button, Card, Checkbox, FormControl, FormHelperText, Icon, IconButton, Input, InputLabel, SvgIcon, TextField, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link, Route, Switch
+} from "react-router-dom";
+import Materials from './Components/Materials';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
 const App = () => {
     return (
         <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
+            <Router>
+                <nav >
+                    <ul style={{
+                        display: 'flex', listStyleType: 'none', position: 'relative',
+                        height: '100px',
+                        width: '100%'
+                    }}>
+                        <li style={{ paddingRight: '1rem' }} >
+                            <Link to="/">SignUp</Link>
+                        </li>
+                        <li style={{ paddingRight: '1rem' }}>
+                            <Link to="/SignIn">SignIn</Link>
+                        </li>
+                        <li style={{ paddingRight: '1rem' }}>
+                            <Link to="/materials">Materials</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
 
-                    </IconButton>
-                    <Typography variant="h6" >
-                        News
-                    </Typography>
-                    <Icon color="action" ></Icon>
+                    <Route path="/materials">
+                        <Materials />
+                    </Route>
+                    <Route path="/SignIn">
+                        <SignIn />
+                    </Route>
+                    <Route path="/">
+                        <SignUp />
+                    </Route>
+                </Switch>
 
-                    <Avatar>S</Avatar>
-                    <Icon>home</Icon>
-                    <Icon>notifications</Icon><Icon>settings</Icon><Icon>star</Icon>
-                    <SvgIcon>
-                        <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
-                    </SvgIcon>
-                </Toolbar>
-            </AppBar>
-            <Card>
-
-                <FormControl>
-                    <InputLabel htmlFor="my-input">Email address</InputLabel>
-                    <Input id="my-input" aria-describedby="my-helper-text" />
-                    <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-                    <Button variant="contained" color="primary">Login</Button>
-                </FormControl>
-                <br />
-                <form noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Add" />
-
-                </form>
-                <Checkbox
-                    value="checkedA"
-                    inputProps={{ 'aria-label': 'Checkbox A' }}
-                />
-                <form noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Edit" />
-
-                </form>
-                <Checkbox
-                    value="checkedA"
-                    inputProps={{ 'aria-label': 'Checkbox A' }}
-                />
-                <form noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Delete" />
-
-                </form>
-                <Checkbox
-                    value="checkedA"
-                    inputProps={{ 'aria-label': 'Checkbox A' }}
-                />
-            </Card>
+            </Router>
 
         </div>
 
---
+++ src/Components/Materials.js
@@ -0,0 +1,63 @@
+import { AppBar, Avatar, Button, Card, Checkbox, FormControl, FormHelperText, Icon, IconButton, Input, InputLabel, SvgIcon, TextField, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+
+const Materials = () => {
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        A
+                    </IconButton>
+                    <Typography variant="h6" >
+                        News
+                    </Typography>
+                    <Icon color="action" ></Icon>
+
+                    <Avatar>S</Avatar>
+
+                    <Icon>home</Icon>
+                    <Icon>notifications</Icon><Icon>settings</Icon><Icon>star</Icon>
+                    <SvgIcon>
+                        <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
+                    </SvgIcon>
+                </Toolbar>
+            </AppBar>
+            <Card>
+
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Email address</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                    <Button variant="contained" color="primary">Login</Button>
+                </FormControl>
+                <br />
+                <form noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Name" />
+
+                </form>
+                <Checkbox
+                    value="checkedA"
+                />
+                <form noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Age" />
+
+                </form>
+                <Checkbox
+                    value="checkedA"
+                    inputProps={{ 'aria-label': 'Checkbox A' }}
+                />
+                <form noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Profession" />
+
+                </form>
+                <Checkbox
+                    value="checkedA"
+                    inputProps={{ 'aria-label': 'Checkbox A' }}
+                />
+            </Card>
+        </div>
+    )
+}
+
+export default Materials
